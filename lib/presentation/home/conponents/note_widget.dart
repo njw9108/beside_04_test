e572d19e14b17ad7dd292d0d33b6173a2de528e2
JINWOO <njw9108@naver.com>
2022-11-12 17:34:25 +0900

    snacbar 추가

--- lib/presentation/home/conponents/note_widget.dart
+++ lib/presentation/home/conponents/note_widget.dart
@@ -1,5 +1,6 @@
 import 'package:beside04_test/domain/model/note.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NoteWidget extends StatelessWidget {
   final Note note;
@@ -15,6 +16,9 @@ class NoteWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    DateTime date = DateTime.fromMillisecondsSinceEpoch(note.timestamp);
+    DateFormat format = DateFormat('yy/MM/dd HH:mm:ss');
+
     return InkWell(
       onTap: () async {
         await noteTap(note);
@@ -35,6 +39,13 @@ class NoteWidget extends StatelessWidget {
               style: const TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              format.format(date),
+              textAlign: TextAlign.end,
+              style: const TextStyle(
+                color: Colors.black45,
               ),
             ),
             const SizedBox(
